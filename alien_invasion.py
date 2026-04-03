22bb65e34efe8735f712ab32a7580c5b6ec74141
Nitya-shukla <nityashukla444@gmail.com>
2026-04-03 16:29:12 +0530

    added alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,8 +32,6 @@ class AlienInvasion :
             self._update_bullets()
             self._update_screen()
 
-           
-  
             
     def _check_events(self):
         for event in pygame.event.get():
